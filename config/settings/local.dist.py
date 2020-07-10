bdb15a66704d1098b9f73a2c44aa3798e6758dcc
degrijo <yarikyatsenko@mail.ru>
2020-07-10 17:25:54 +0300

    update local.dist

--- config/settings/local.dist.py
+++ config/settings/local.dist.py
@@ -1,13 +1,13 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ALLOWED_HOSTS = []
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
 SECRET_KEY = ''
 DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
